bb48360841b0b1faf20efc1742dbf0c31c16f812
VaLeRo <valdirlealrocha@hotmail.com>
2024-05-28 16:47:12 -0300

    Primeira atualização das aulas do Curso de Python
    Atualização das aulas do Curso de Python

---
+++ CFBCursosPythonAula01.py
@@ -0,0 +1,11 @@
+# Aula 01 - https://youtu.be/Ay-MakuSg08?list=PLx4x_zx8csUhuVgWfy7keQQAy7t1J35TR
+# Instalação e primeiro programa
+# Na aula de hoje do curso de Python vamos aprender como instalar e 
+# preparar o ambiente para desenvolvimento e criar nosso primeiro 
+# programa em Python.
+# Python é uma linguagem de programação que permite trabalhar 
+# rapidamente e integrar sistemas de forma mais eficaz.
+# Python não é uma linguagem compilada e sim interpretada, 
+# também não é fortemente tipada o que o torna uma linguagem 
+# de fácil aprendizado e rápida produtividade.
+print("CFB Cursos")
